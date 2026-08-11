9e73e0aa7cd3d6d4f406a860c3c07d0751d580d2
pikachu <bigfootamoeba6@gmail.com>
2026-08-11 21:44:57 +0530

    Add palindromic-substrings.java

---
+++ palindromic-substrings.java
@@ -0,0 +1,32 @@
+class Solution {
+
+    public int countSubstrings(String s) {
+
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            count += expand(s, i, i);       // odd
+            count += expand(s, i, i + 1);   // even
+        }
+
+        return count;
+    }
+
+    private int expand(String s, int left, int right) {
+
+        int count = 0;
+
+        while (left >= 0 &&
+               right < s.length() &&
+               s.charAt(left) == s.charAt(right)) {
+
+            count++;
+
+            left--;
+            right++;
+        }
+
+        return count;
+    }
+}
